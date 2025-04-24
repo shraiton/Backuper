f55d1d0013c178bf224a45b61058ffcc8bc3b3e9
gitea <gitea>
2025-04-24 21:31:00 +0330

    fix

--- backuper.sh
+++ backuper.sh
@@ -635,7 +635,7 @@ marzban_template() {
 
     if [[ "$db_type" == "sqlite3" ]]; then
         BACKUP_DB_COMMAND="sqlite3 /var/lib/marzban/db.sqlite3 .dump > $DB_PATH.sql" #"mysqldump -h $db_host -P $db_port -u $db_user -p'$db_password' '$db_name' > $DB_PATH"
-        DIRECTORIES+=($DB_PATH)
+        DIRECTORIES+=($DB_PATH.sql)
     fi
 
     # Export backup variables
